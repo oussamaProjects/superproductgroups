390237390e476dc8c54d980a8b672d3527dcaac9
Oussama - MBA m1 <slashnpro@gmail.com>
2025-02-11 00:00:48 +0100

    fix

--- src/Form/Modifier/ProductFormModifier.php
+++ src/Form/Modifier/ProductFormModifier.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace PrestaShop\Module\SuperProductGroups\Form\Modifier;
 
 use PrestaShopBundle\Form\FormBuilderModifier;
+use Symfony\Component\Form\FormBuilderInterface;
+use PrestaShop\PrestaShop\Adapter\Entity\DbQuery;
+use PrestaShop\Module\SuperProductGroups\Form\Type\GroupFormType;
 
 final class ProductFormModifier
 {
@@ -22,4 +25,36 @@ final class ProductFormModifier
     {
         $this->formBuilderModifier = $formBuilderModifier;
     }
+
+    // /**
+    //  * Modifies the product form by adding a group selection field.
+    //  *
+    //  * @param int $productId The ID of the product being edited
+    //  * @param FormBuilderInterface $productFormBuilder The form builder for the product
+    //  */
+    // public function modify(int $productId, FormBuilderInterface $productFormBuilder): void
+    // {
+    //     $this->addCustomTab($productId, $productFormBuilder);
+    // }
+
+
+    // /**
+    //  * @param CustomProduct $customProduct
+    //  * @param FormBuilderInterface $productFormBuilder
+    //  */
+    // private function addCustomTab(int $productId, FormBuilderInterface $productFormBuilder): void
+    // {
+
+    //     // Add a multiple-choice field to select groups (name, image) associated with the products in the form builder using the form builder modifier
+
+    //     $this->formBuilderModifier->addAfter(
+    //         $productFormBuilder,
+    //         'description',
+    //         'the_group_tab',
+    //         GroupFormType::class,
+    //         [
+    //             'label' => 'Group Information',
+    //         ]
+    //     );
+    // }
 }
